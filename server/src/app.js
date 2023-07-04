ca18ee4dc57c1a310c2f4b88547221bd23528809
James <66404238+JSulley@users.noreply.github.com>
2023-07-04 14:21:34 -0500

    Used middleware to log front and back end requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router');
-
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
