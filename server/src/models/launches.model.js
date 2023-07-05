7ac1b673b9679e11bfc8b4bac6607c469f5373f5
James <66404238+JSulley@users.noreply.github.com>
2023-07-05 02:45:27 -0500

    Applied separation of concerns
    For launches.model.js, a getter function was defined to abstract the implementation of retrieving the data since the model focuses on the logic related to the data. This function is exported instead of the data itself.
    launches.controller.js makes use of the launches model, so the function from the model is imported. The function defined in the controller has been renamed to "httpGetAllLaunches" because this function works directly with the request and returns the response to the client.
    Since the router is related to the controller, the renamed function from controller has been imported to launches.router.js as needed.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,18 +1,22 @@
 const launches = new Map();
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
-    customers: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true,
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
 module.exports = {
-    launches,
+  getAllLaunches,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const {
-    getAllLaunches,
-} = require('./launches.controller');
+const express = require("express");
+const { httpGetAllLaunches } = require("./launches.controller");
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', getAllLaunches);
+launchesRouter.get("/launches", httpGetAllLaunches);
 
 module.exports = launchesRouter;
