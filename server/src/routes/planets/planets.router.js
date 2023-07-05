1b177392c963f3ec421c0a166b76b6188ff68517
James <66404238+JSulley@users.noreply.github.com>
2023-07-05 03:09:25 -0500

    Applied separation of concerns to planets endpoint

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 
-const {
-    getAllPlanets,
-} = require('./planets.controller');
+const { httpGetAllPlanets } = require("./planets.controller");
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', getAllPlanets);
+planetsRouter.get("/planets", httpGetAllPlanets);
 
 module.exports = planetsRouter;
