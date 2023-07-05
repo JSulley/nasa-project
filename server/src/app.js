cf395edfeb3f798bd2b165ae9bd682711a952669
James <66404238+JSulley@users.noreply.github.com>
2023-07-05 00:25:09 -0500

    Added app middleware for launches collection

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 const app = express();
 
 app.use(cors({
@@ -15,7 +16,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
