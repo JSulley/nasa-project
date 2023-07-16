7797ff68e699e067435743cd012ddc8a85ad0f3d
James <66404238+JSulley@users.noreply.github.com>
2023-07-15 21:22:46 -0500

    Mounted launches, planets URL on app middleware

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -4,6 +4,6 @@ const { httpGetAllPlanets } = require("./planets.controller");
 
 const planetsRouter = express.Router();
 
-planetsRouter.get("/planets", httpGetAllPlanets);
+planetsRouter.get("/", httpGetAllPlanets);
 
 module.exports = planetsRouter;
